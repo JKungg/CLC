16713a421acea548e9ce0deae4f541d21557d1a0
JKungg <jackdilkens@gmail.com>
2022-10-19 23:15:18 -0400

    Added Required Comments Added Statements for other options

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main() {
     // Print Calculator Welcome Message
     printf("Welcome to my Command-Line Calculator (CLC)\n");
     printf("Developer: ");
+    // Read Developer Name
     scanf("%s", name);
     printf("Version: 1\n");
     printf("Date: 2022-09-26\n");
@@ -26,38 +27,48 @@ int main() {
     // While Loop to keep calculator running until user wants to exit.
     while(toupper(userInput) != 'E'){
 
-        // Read User Selection
+        // Show Option selection message
         printf("\n----------------------------------------------------------"
                "\nPlease select your option ( B , U , A , V , E ) : ");
+        // Read userInput
         scanf("%s", &userInput);
 
         // Convert userInput to uppercase.
+        // If userInput is 'B'
         if(toupper(userInput) == 'B'){
 
-            // Read num1, num2 and operator
+            // Declare required variables
             double num1, num2;
             char operator;
+
             printf("Please enter the first number: ");
+            // Read num1
             scanf("%lf", &num1);
             printf("Please enter the operation ( + , - , * , /) : ");
+            // Read operator
             scanf("%s", &operator);
             printf("Please enter the second number: ");
+            // Read num2
             scanf("%lf", &num2);
 
             // Switch case for the 4 types of possible operations
             switch(operator){
                 case '+' :
+                    // Calculate sum and print it.
                     printf("The result is %lf", (num1+num2));
                     break;
 
                 case '-' :
+                    // Calculate subtraction and print it.
                     printf("The result is %lf", (num1-num2));
                     break;
 
                 case '*' :
+                    // Calculate product and print it.
                     printf("The result is %lf", (num1*num2));
                     break;
 
+                    // Calculate Quotient and print it
                 case '/' :
                     // Must make sure it is not trying to be divided by 0.
                     if(num2!=0.00){
@@ -72,6 +83,15 @@ int main() {
                 default :
                     printf("\nSorry I don't have enough knowledge to serve you at this time.");
             }
+            // If statement for option U
+        } else if(userInput=='U'){
+            printf("Sorry I don't have enough knowledge to serve you at this time.");
+            // If statement for option A
+        } else if(userInput=='A'){
+            printf("Sorry I don't have enough knowledge to serve you at this time.");
+            // If statement for option V
+        } else if(userInput=='V'){
+            printf("Sorry I don't have enough knowledge to serve you at this time.");
         }
     }
 
