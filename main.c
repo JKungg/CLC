80aa23df1e3a7782c9c1472e75a59230cf074fdf
JKungg <jackdilkens@gmail.com>
2022-11-08 20:44:59 -0500

    Binary + Remainder + Power + Maximum, Minimum Unary + Square root + Logarithm + Exponentiation + Floor, Ceiling

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 
 int main() {
     // Declare Variables for - Developer Name and UserInput
@@ -38,46 +39,65 @@ int main() {
         if(toupper(userInput) == 'B'){
 
             // Declare required variables
-            double num1, num2;
+            float num1, num2;
             char operator;
 
-            printf("Please enter the first number: ");
+            printf("Please enter the first number:");
             // Read num1
-            scanf("%lf", &num1);
-            printf("Please enter the operation ( + , - , * , /) : ");
+            scanf("%f", &num1);
+            printf("Please enter the operation ( + , - , * , /, %, P, X, I) :");
             // Read operator
             scanf("%s", &operator);
-            printf("Please enter the second number: ");
+            printf("Please enter the second number:");
             // Read num2
-            scanf("%lf", &num2);
+            scanf("%f", &num2);
 
             // Switch case for the 4 types of possible operations
             switch(operator){
                 case '+' :
                     // Calculate sum and print it.
-                    printf("The result is %lf", (num1+num2));
+                    printf("The result is %f", (num1+num2));
                     break;
 
                 case '-' :
                     // Calculate subtraction and print it.
-                    printf("The result is %lf", (num1-num2));
+                    printf("The result is %f", (num1-num2));
                     break;
 
                 case '*' :
                     // Calculate product and print it.
-                    printf("The result is %lf", (num1*num2));
+                    printf("The result is %f", (num1*num2));
                     break;
 
                     // Calculate Quotient and print it
                 case '/' :
                     // Must make sure it is not trying to be divided by 0.
                     if(num2!=0.00){
-                        printf("The result is %lf", (num1/num2));
+                        printf("The result is %f", (num1/num2));
                         break;
                     } else{
                         printf("You cannot divide by 0!");
                         break;
                     }
+                    // Calculate Remainder
+                case '%':
+                    printf("The result is %f", remainderf(num1,num2));
+                    break;
+
+                    // Calculate Power
+                case 'P':
+                    printf("The result is %f", pow(num1,num2));
+                    break;
+
+                    // Calculate Maximum
+                case 'X':
+                    printf("The result is %f", fmaxf(num1,num2));
+                    break;
+
+                    // Calculate Minimum
+                case 'I':
+                    printf("The result is %f", fminf(num1,num2));
+                    break;
 
                     // Default case if user doesn't select one of the operators available.
                 default :
@@ -85,7 +105,53 @@ int main() {
             }
             // If statement for option U
         } else if(userInput=='U'){
-            printf("Sorry I don't have enough knowledge to serve you at this time.");
+            float num;
+            char operator;
+            printf("Enter a number:");
+            scanf("%f", &num);
+            printf("Enter an operator(S for square root, L for logarithm (logx), E for exponentiation (e!), C for the smallest integer value greater than or equal to x, or F for the largest integer value greater than or equal to x.)");
+            scanf("%s", &operator);
+
+            // Switch for the different possible operands.
+            switch(operator){
+
+                // Case for square root.
+                case 'S':
+                    // Check to see if it is a positive number
+                    if(num >= 0){
+                        printf("The result is: %f", sqrtf(num));
+                        break;
+                    } else{
+                        printf("Invalid input!");
+                        break;
+                    }
+
+                    // Case for Logarithm
+                case 'L':
+                    if(num>0){
+                        printf("The result is: %f", logf(num));
+                        break;
+                    } else{
+                        printf("Invalid input!");
+                        break;
+                    }
+
+                    // Case for exponentiation
+                case 'E':
+                    printf("The result is: %f", expf(num));
+                    break;
+
+                    // Case for smallest integer value greater than or equal to x
+                case 'C':
+                    printf("The result is: %f", ceilf(num));
+                    break;
+
+                    // Case for largest integer value greater than or equal to x
+                case 'F':
+                    printf("The result is: %f", floorf(num));
+                    break;
+
+            }
             // If statement for option A
         } else if(userInput=='A'){
             printf("Sorry I don't have enough knowledge to serve you at this time.");
@@ -93,6 +159,7 @@ int main() {
         } else if(userInput=='V'){
             printf("Sorry I don't have enough knowledge to serve you at this time.");
         }
+
     }
 
 
