9e8f8c1e88b4c62a67cf524579dba50c1349bf77
Jack Dilkens <jackdilkens@gmail.com>
2022-11-19 21:06:24 -0500

    Added checks for multiple inputs. Added check for variable or number input for Advanced calculations. Completed Advanced Operations tab.
    Assignement Part 2 Final.

--- main.c
+++ main.c
@@ -11,8 +11,10 @@ int checkValidFloat(float num){
     if(fgets(buffer, sizeof(buffer), stdin) == NULL)
         return -1;
     num = strtod(buffer, &endptr);
+    //Check if it's a float and return 1 if True
     if((*endptr == '\0') || (isspace(*endptr) != 0))
         return 1;
+    //If it is not a true float return 0.
     else
         return 0;
 }
@@ -20,11 +22,59 @@ int checkValidFloat(float num){
 float getNum(){
     float num;
     scanf("%f", &num);
-    while(checkValidFloat(num) == 0){
+    while(checkValidFloat(num) != 1){
         printf("Invalid Input. Enter a number: ");
         scanf("%f", &num);
     }
     return num;
+}
+
+float a = 0, b = 0, c = 0, d =0, e = 0;
+
+float getVar(char var){
+    switch(var){
+        case 'a':
+            return a;
+        case 'b':
+            return b;
+        case 'c':
+            return c;
+        case 'd':
+            return d;
+        case 'e':
+            return e;
+    }
+}
+
+float varOrNum(){
+    char in, varA;
+    //Ask user if they want to input a variable or number
+    printf("Would you like to input a variable(V) or a number(N)?: ");
+    scanf("%s", &in);
+    // Loop until proper input is entered
+    while(toupper(in)!='V'&& toupper(in)!='N'){
+        printf("Invalid input, would you like to input a Variable(V) or Number(N)?: ");
+        scanf("%s", &in);
+    }
+
+    // Variable choice
+    if(toupper(in)=='V'){
+        //Get operator and variables from user.
+        printf("Please enter the first variable:");
+        scanf("%s", &varA);
+        //Loop until proper input is entered.
+        while(varA != 'a' && varA != 'b' && varA != 'c' && varA != 'd' && varA != 'e'){
+            printf("Invalid variable. Enter a variable letter (a-e): ");
+            scanf("%s", &varA);
+        }
+        return getVar(varA);
+    }
+        // Number choice
+    else if(toupper(in)=='N'){
+        //Get operator and numbers from user.
+        printf("Please enter a number:");
+        return getNum();
+    }
 }
 
 // Function for Binary Operations
@@ -48,13 +98,12 @@ float binaryOperation(char operator, float num1, float num2){
             // Calculate Quotient and print it
         case '/' :
             // Must make sure it is not trying to be divided by 0.
-            if(num2!=0.00){
-                printf("The result is %f", (num1/num2));
-                return(num1/num2);
-            } else{
-                printf("You cannot divide by 0!");
-                break;
+            while(num2==0){
+                printf("Denominator cannot be 0, enter another number: ");
+                num2 = getNum();
             }
+            printf("The result is %f", (num1/num2));
+            return(num1/num2);
             // Calculate Remainder
         case '%':
             printf("The result is %f", remainderf(num1,num2));
@@ -88,24 +137,27 @@ float unaryOperation(char operator, float num){
 
         // Case for square root.
         case 'S':
+            while(num<0){
+                printf("Number cannot be negative, enter another number: ");
+                num = getNum();
+            }
             // Check to see if it is a positive number
             if(num >= 0){
                 printf("The result is: %f", sqrtf(num));
                 return(sqrtf(num));
             } else{
-                printf("Invalid input!");
+                printf("You cannot get the squareroot of a negative number!");
                 break;
             }
 
             // Case for Logarithm
         case 'L':
-            if(num>0){
+            while(num<=0){
+                printf("Number cannot be negative or zero, enter another number: ");
+                num = getNum();
+            }
                 printf("The result is: %f", logf(num));
                 return logf(num);
-            } else{
-                printf("Invalid input!");
-                break;
-            }
 
             // Case for exponentiation
         case 'E':
@@ -125,7 +177,6 @@ float unaryOperation(char operator, float num){
     }
 }
 
-float a = 0, b = 0, c = 0, d =0, e = 0;
 
 int main() {
     // Declare Variable for UserInput
@@ -133,7 +184,7 @@ int main() {
 
     // Print Calculator Welcome Message
     printf("Welcome to my Command-Line Calculator (CLC)\n");
-    printf("Developer: Jack Dilkens");
+    printf("Developer: Jack Dilkens\n");
     printf("Version: 2\n");
     printf("Date: 2022-11-18\n");
     printf("----------------------------------------------------------\n\n");
@@ -160,12 +211,13 @@ int main() {
 
             // Declare required variables
             float num1, num2;
-            char op;
+            char op = 0;
             //Get operator and numbers from user.
             printf("Please enter the first number:");
             num1 = getNum();
+            //Loop until proper input is entered.
             while(op!='+' && op!='-' && op!='*'&& op!='/' && op!='%' && op!= 'P' && op!='X' && op!='I'){
-                printf("Please enter the operation ( + , - , * , /, %, P, X, I) :");
+                printf("Please enter the operation ( + , - , *, /, %%, P, X, I) :");
                 scanf("%s", &op);
             }
             printf("Please enter the second number:");
@@ -178,12 +230,13 @@ int main() {
         } else if(toupper(userInput)=='U'){
             //Declare required variables.
             float num;
-            char op;
+            char op = 0;
             //Get operator and number from user.
             printf("Enter a number:");
             num = getNum();
+            //Loop until proper input is entered.
             while(op!='S' && op!='L' && op!='E'&& op!='C' && op!='F'){
-                printf("Enter an operator(S for square root, L for logarithm (logx), E for exponentiation (e!), C for the smallest integer value greater than or equal to x, or F for the largest integer value greater than or equal to x.)");
+                printf("\nEnter an operator...\nS for square root...\nL for logarithm (logx)...\nE for exponentiation (e!)...\nC for the smallest integer value greater than or equal to x...\nF for the largest integer value greater than or equal to x...: ");
                 scanf("%s", &op);
             }
 
@@ -196,11 +249,12 @@ int main() {
             char userVar;
             printf("Enter a variable letter (a-e):");
             scanf("%s", &userVar);
+            //Loop until proper input is entered.
             while(userVar != 'a' && userVar != 'b' && userVar != 'c' && userVar != 'd' && userVar != 'e'){
                 printf("Invalid variable. Enter a variable letter (a-e): ");
                 scanf("%s", &userVar);
             }
-
+            // Switch for assigning a value to the variable.
             switch(userVar){
                 case 'a':
                     printf("Enter a number for the variable %c:",userVar);
@@ -227,17 +281,51 @@ int main() {
             }
 
             // If statement for option A
-        } else if(userInput=='A'){
-            char selection;
-            printf("Select an option (B; Binary Operation, U; Unary Operation, E; Exit):");
-            scanf("%s",&selection);
-            if(selection=='B'){
+        } else if(toupper(userInput)=='A'){
+            char selection = 0;
+            while(toupper(selection)!='E'){
+                printf("\nSelect an option (B; Binary Operation, U; Unary Operation, E; Exit):");
+                scanf("%s",&selection);
 
+                // Advanced Binary Operations
+                if(toupper(selection)=='B'){
+                    // Declare required variables
+                    char op = 0;
+                    float num1, num2;
+                    // Assign the value of the variable or number to num1
+                    num1 = varOrNum();
+
+                    //Get Operator
+                    //Loop until proper input is entered.
+                    while(op!='+' && op!='-' && op!='*'&& op!='/' && op!='%' && op!= 'P' && op!='X' && op!='I'){
+                        printf("Please enter the operation ( + , - , * , /, %%, P, X, I) :");
+                        scanf("%s", &op);
+                    }
+                    // Assign the value of the variable or number to num2
+                    num2 = varOrNum();
+
+                    //Call Binary Operation Function.
+                    binaryOperation(op,num1,num2);
+
+                }
+                // Advanced Unary Operation
+                else if(toupper(selection)=='U'){
+                    //Declare required variables.
+                    float num;
+                    char op = 0, var = 0;
+
+                    // Assign the value of the variable or number to num
+                    num = varOrNum();
+
+                    while(op!='S' && op!='L' && op!='E'&& op!='C' && op!='F'){
+                        printf("Enter an operator...\nS for square root...\nL for logarithm (logx)...\nE for exponentiation (e!)...\nC for the smallest integer value greater than or equal to x...\nF for the largest integer value greater than or equal to x...: ");
+                        scanf("%s", &op);
+                    }
+                    // Call unary operation
+                    unaryOperation(op,num);
+                }
             }
         }
-
     }
-
-
     return 0;
 }
